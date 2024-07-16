0012a1fa26d46cbf8c2781d307366c7a0720398c
Mateus Silva <malockeybr@gmail.com>
2024-07-16 15:30:00 -0300

    feat: add update product

--- app.py
+++ app.py
@@ -43,6 +43,26 @@ def get_produt_details(product_id):
         }), 200
     return jsonify({'message': 'Product not found!'}), 404
 
+@app.route('/api/products/update/<int:product_id>', methods=['PUT'])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({'message': 'Product not found!'}), 404
+    
+    data = request.json
+    if 'name' in data:
+        product.name = data['name']
+        
+    if 'price' in data:
+        product.price = data['price']
+        
+    if 'description' in data:
+        product.description = data['description']
+        
+    db.session.commit()
+    
+    return jsonify({'message': 'Product updated!'}), 200
+    
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
